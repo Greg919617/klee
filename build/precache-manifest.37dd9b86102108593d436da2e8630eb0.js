927d4fd791fe4a0e0c73bcde4d4d6f6d6e72fb10
Greg Jones <gtjones1@aggies.ncat.edu>
2019-06-24 23:04:29 -0500

    ran command rm -rf node_modules, then npm install react-scripts@1.1.1, then npm install

---
+++ build/precache-manifest.37dd9b86102108593d436da2e8630eb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfa07fac33697734fdcd9c8410a885b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32e050ef1d04c047269a",
+    "url": "./static/css/2.2ef9cc1e.chunk.css"
+  },
+  {
+    "revision": "9cb80c3f406fd364852b",
+    "url": "./static/css/main.13333688.chunk.css"
+  },
+  {
+    "revision": "32e050ef1d04c047269a",
+    "url": "./static/js/2.c6f6eea8.chunk.js"
+  },
+  {
+    "revision": "9cb80c3f406fd364852b",
+    "url": "./static/js/main.57281909.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
